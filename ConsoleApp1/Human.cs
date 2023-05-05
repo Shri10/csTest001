dae367f3f534e2fd86b9162dfb9b781f066ec722
Shri10 <pethe.shriraj@gmail.com>
2023-05-05 15:20:01 +0530

    [Shri10] New Branch

--- ConsoleApp1/Human.cs
+++ ConsoleApp1/Human.cs
@@ -9,5 +9,16 @@ namespace ConsoleApp1
     class Human
     {
         //This is another Branch
+        public string name;
+        public float height;
+        public long phone;
+        public void Talk()
+        {
+            Console.WriteLine("Hi, my name is  " + name);
+        }
+        public void HumanDetails()
+        {
+            Console.WriteLine("Name: {0}, height: {1}, Phone number: {2}", name, height, phone);
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,10 +11,22 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Greetings Universe !!!");
-            Console.WriteLine("This is Shriraj.");
+
+            Human human1 = new Human();
+
+            Console.WriteLine("Please enter your Name:");
+            human1.name = Console.ReadLine();
+            Console.WriteLine("Please enter your Height:");
+            human1.height = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Please enter your Phone Number:");
+            human1.phone = long.Parse(Console.ReadLine());
+
+            human1.HumanDetails();
+
+            /*Console.WriteLine("This is Shriraj.");
             Console.WriteLine("A participant of BL.");
             Console.WriteLine("Remote line modified.");
-            Console.WriteLine("Modified in local.");
+            Console.WriteLine("Modified in local.");*/
             Console.ReadLine();
         }
     }
